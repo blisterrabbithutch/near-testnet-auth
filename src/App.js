16246f856fad2f6c50c7a7a2a6cb18ae23e9310f
Denis Chizh <denischizh@MacBook-Pro-Denis.local>
2022-02-27 13:12:00 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import './App.scss';
+import { useEffect, useState } from "react";
+import * as nearAPI from "near-api-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const { connect, keyStores, WalletConnection } = nearAPI;
+
+const config = {
+    networkId: "testnet",
+    keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+};
+
+const App = () => {
+    const [walletInstance, setWalletInstance] = useState(undefined);
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    const [userId, setUserId] = useState('');
+
+    useEffect(() => {
+        const initNearInstances = async () => {
+            const near = await connect(config);
+            const wallet = await new WalletConnection(near);
+            setWalletInstance(wallet);
+            setUserId(wallet.account().accountId);
+        }
+
+        initNearInstances().catch(console.log);
+    }, []);
+
+    useEffect(() => {
+        if (walletInstance === undefined) {
+            return;
+        }
+        if (walletInstance.isSignedIn()) {
+            setIsSignedIn(true);
+        } else {
+            setIsSignedIn(false);
+        }
+    }, [walletInstance]);
+
+    const handleAuthUser = async () => {
+        if (isSignedIn) {
+            walletInstance.signOut();
+            setIsSignedIn(false);
+        } else {
+            await walletInstance.requestSignIn(
+                'example-contract.testnet', // contract requesting access
+                "Test DApp",
+            ).catch(console.log);
+        }
+    }
+
+    const handleClick = async () => {
+        await handleAuthUser();
+    }
+
+    return (
+        <div className="app">
+            <div className="app__container">
+                <Typography variant="subtitle1" gutterBottom component="div">
+                    { isSignedIn ? `Connected, ${userId}` : 'Connect'}
+                </Typography>
+                <Button variant="contained" onClick={handleClick}>
+                    { isSignedIn ? 'Disconnect' : 'Connect'}
+                </Button>
+            </div>
+        </div>
   );
 }
 
